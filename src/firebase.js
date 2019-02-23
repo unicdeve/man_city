c1d286f13229943d4a0889e38f88b525f73ff3c4
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-02-23 13:17:20 +0100

    the tags -- Matches and See more matches tags

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+
+
+var config = {
+    apiKey: "AIzaSyDshjkVEYPtkKTU41HAHfHQm4FG3OIFR9c",
+    authDomain: "m-city-ec27d.firebaseapp.com",
+    databaseURL: "https://m-city-ec27d.firebaseio.com",
+    projectId: "m-city-ec27d",
+    storageBucket: "m-city-ec27d.appspot.com",
+    messagingSenderId: "715124613255"
+  };
+
+  firebase.initializeApp(config);
