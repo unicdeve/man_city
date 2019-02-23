c850e5cbcd9d21860926c39eba70e24376544590
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-02-23 01:49:23 +0100

    The icon component, and added the logo_icon component to the header and done with the header

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -5,6 +5,8 @@ import Button from '@material-ui/core/Button';
 
 import { Link } from 'react-router-dom';
 
+import { CityLogo } from '../ui/icons';
+
 export default class Header extends Component {
   render() {
     return (
@@ -20,7 +22,12 @@ export default class Header extends Component {
         <Toolbar style={{display:'flex'}}>
             <div style={{flexGrow: 1}}>
                 <div className="header_logo">
-                    LOGO
+                    <CityLogo 
+                      link={true}
+                      linkTo='/'
+                      width='70px'
+                      height='70px'
+                    />
                 </div>
             </div>
 
