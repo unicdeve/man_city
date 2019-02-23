8d348bfab3fabeecf7269e05d889dd8e00efc3f6
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-02-23 23:36:46 +0100

    home Meet the player component

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Featured from './featured';
 import Matches from './matches';
+import MeetPlayer from './meetPlayers';
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured />
             <Matches />
+            <MeetPlayer />
         </div>
     );
 };
