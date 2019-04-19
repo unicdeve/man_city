6aa017c10cdf583257e6b591daf9349e3505e3dc
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-04-19 03:37:24 +0100

    FormField component, updateForm func, ++form validation

---
+++ src/Components/home/promotion/Enroll.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import FormField from '../../ui/formFields';
+import { validate } from '../../ui/misc'
+
+export default class Enroll extends Component {
+
+  state = {
+    formError: false,
+    formSuccess: '',
+    formdata: {
+      email: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'email_input',
+          type: 'email',
+          placeholder: "Enter your email"
+        },
+        validation: {
+          required: true,
+          email: true
+        },
+        valid: false,
+        validationMessage: ''
+      }
+    }
+  }
+
+  updateForm(element) {
+    const newFormdata = { ...this.state.formdata };
+    const newElement = { ...newFormdata[element.id] };
+
+    newElement.value = element.event.target.value;
+    
+    let validData = validate(newElement);
+    newElement.valid = validData[0];
+    newElement.validationMessage = validData[1];
+    
+    newFormdata[element.id] = newElement;
+    console.log(newElement)
+    this.setState({
+      formdata: newFormdata
+    });
+  }
+
+  submitForm() {
+
+  }
+
+  render() {
+    return (
+      <Fade>
+        <div className="enroll_wrapper">
+          <form onSubmit={(event) => this.submitForm(event)}>
+            <div className="enroll_title">
+              Enter your name
+            </div>
+
+            <div className="enroll_input">
+              <FormField 
+                id={'email'}
+                formdata={this.state.formdata.email}
+                change={(element) => this.updateForm(element)}
+              />
+            </div>
+          </form>
+        </div>
+      </Fade>
+    )
+  }
+}
