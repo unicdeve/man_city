dc20d1c232d03bdc785d2e37933942cfedbcd0a9
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-04-19 11:08:39 +0100

    users authenticated and redirected to dashboard

--- src/Components/signin/index.js
+++ src/Components/signin/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { firebase } from '../../firebase';
+
 import FormField from '../ui/formFields';
 import { validate } from '../ui/misc';
 
@@ -71,7 +73,18 @@ export default class SignIn extends Component {
     }
 
     if(formIsValid){
-      console.log(dataToSubmit)
+      firebase.auth()
+      .signInWithEmailAndPassword(
+        dataToSubmit.email,
+        dataToSubmit.password
+      ).then(() => {
+        this.props.history.push('/dashboard');
+      }).catch(error => {
+        this.setState({
+          formError: true
+        })
+      })
+
     } else {
       this.setState({
         formError: true
@@ -97,6 +110,11 @@ export default class SignIn extends Component {
               formdata={this.state.formdata.password}
               change={(element) => this.updateForm(element)}
             />
+
+            { this.state.formError ? 
+              (<div className="error_label">Something is wrong, try again!!</div>)
+              : null
+            }
 
             <button onClick={(event) => this.submitForm(event)}>Login</button>
 
